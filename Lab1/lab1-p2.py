e4b977e2bd1f0318b0b81224f9a848c53bce3ac4
Eric Francisco Ponce <eric.48p@gmail.com>
2024-02-13 17:19:08 -0800

    Started Lab 3

--- Lab1/lab1-p2.py
+++ Lab1/lab1-p2.py
@@ -3,7 +3,7 @@ def main():
   count = int(input("How many times would you like to repeat that sentence \n"))
   i = 0
   with open('CompletedPunishment.txt', "w") as file:
-    while i <= count:
+    while i < count:
       file.write(sentence + "\n")
       i += 1
 
